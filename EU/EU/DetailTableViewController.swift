afa645b689b34cfe111d973d34901df00b994c07
johnmekus <85038956+johnmekus@users.noreply.github.com>
2021-09-26 20:06:58 -0400

    added EU side proj

---
+++ EU/EU/DetailTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailTableViewController.swift
+//  EU
+//
+//  Created by John Mekus on 9/24/21.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var countryField: UITextField!
+    @IBOutlet weak var capitalField: UITextField!
+    @IBOutlet weak var euroSwitch: UISwitch!
+    
+    var nation: Nation!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        if nation == nil
+        {
+            nation = Nation(country: "", capital: "", usesEuro: false)
+        }
+        countryField.text = nation.country
+        capitalField.text = nation.capital
+        euroSwitch.isOn = nation.usesEuro
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        nation = Nation(country: countryField.text!, capital: capitalField.text!, usesEuro: euroSwitch.isOn)
+    }
+    
+    @IBAction func saveBarButton(_ sender: UIBarButtonItem)
+    {
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem)
+    {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode
+        {
+            dismiss(animated: true, completion: nil)
+        }
+        else
+        {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
---
+++ EU/EU/Nation.swift
@@ -0,0 +1,15 @@
+//
+//  Nation.swift
+//  EU
+//
+//  Created by John Mekus on 9/26/21.
+//
+
+import Foundation
+
+struct Nation: Codable
+{
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
